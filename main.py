ca52dd331be2492586b17329d3392d5c15753c18
Damian <damianakpan2001@gmail.com>
2020-12-15 12:18:06 +0100

    Added context menus

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5 import QtCore
 from build.main import Ui_MainWindow
 
-class Main(QMainWindow, Ui_MainWindow):
+class Main(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(Main, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.label = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.main_toolBar.addWidget(self.label)
+
+    def contextMenuEvent(self, event, *args, **kwargs):
+        contextMenu = QtWidgets.QMenu(self)
+
+        newAction = contextMenu.addAction('&About')
+        quitAction = contextMenu.addAction('&Quit')
+
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == quitAction:
+            self.close()
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QtWidgets.QApplication([])
     window = Main()
     window.show()
     app.exec_()
